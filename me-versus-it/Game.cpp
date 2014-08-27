49fab90fbc9046f011b1ff3e6a6ada00983374fc
Jonathan <jmuller@chronoresto.com>
2014-08-27 13:48:26 +0200

    Draw test string on screen

--- me-versus-it/Game.cpp
+++ me-versus-it/Game.cpp
@@ -13,6 +13,7 @@ void Game::init(){
 	m_mainFont = TTF_OpenFont("resources/fonts/lato/regular.ttf", 32);
 	if (m_mainFont != nullptr){
 		m_mainDrawSurface = TTF_RenderText_Blended(m_mainFont, "TEST", m_fontColor);
+		
 	}
 	else{
 		std::cout << "TTF_OpenFont error: " << TTF_GetError() << std::endl;
@@ -28,5 +29,10 @@ void Game::update(float delta){
 }
 
 void Game::draw(SDL_Renderer& renderer){
-
+	m_mainDrawTexture = SDL_CreateTextureFromSurface(&renderer, m_mainDrawSurface);
+	int iW, iH;
+	SDL_QueryTexture(m_mainDrawTexture, NULL, NULL, &iW, &iH);
+	SDL_Rect dst = { 0, 0, iW, iH };
+	SDL_RenderCopy(&renderer, m_mainDrawTexture, NULL, &dst);
+	SDL_DestroyTexture(m_mainDrawTexture);
 }
